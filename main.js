3096616138680c2cd18ab5d372803e182451b332
Madison Raquet <mraquet@mosaic-compliance.com>
2018-08-17 11:45:46 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        title: 'What is on your todo list today?',
+        task: {
+            text: ''
+        },
+        tasks: [
+            {
+                text: 'Finish Vue Udemy Course',
+                date: new Date(Date.now()).toLocaleString()
+            },
+            {
+                text: 'Get a haircut'
+            },
+            {
+                text: 'Call mom'
+            }
+
+        ],
+        numberCompleted: 0
+
+    },
+    methods: {
+        addTask() {
+            let {text} = this.task
+            this.tasks.push({text,
+                date: new Date(Date.now()).toLocaleString()
+            })
+        },
+        removeTask(index) {
+            this.tasks.splice(index, 1);
+        },
+
+        completeTask() {
+            var checkBox = document.getElementById('checkedBox');
+            var completedTasks = document.getElementById('taskNumber');
+            if (checkBox.checked == true){
+                this.numberCompleted++
+            } else if (checkBox.checked == false){
+                this.numberCompleted--
+            };
+        }
+    }
+});
